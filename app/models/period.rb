4493fe0edf714b01a75233951202890b97c4c6d5
Jack Marston <jackdyett@gmail.com>
2018-10-20 19:07:53 -0400

    add Period model, table, associations

---
+++ app/models/period.rb
@@ -0,0 +1,4 @@
+class Period < ApplicationRecord
+  belongs_to :plan
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_20_223455) do
+ActiveRecord::Schema.define(version: 2018_10_20_225810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2018_10_20_223455) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_assessments_on_user_id"
+  end
+
+  create_table "periods", force: :cascade do |t|
+    t.bigint "plan_id"
+    t.string "name"
+    t.index ["plan_id"], name: "index_periods_on_plan_id"
   end
 
   create_table "plans", force: :cascade do |t|
